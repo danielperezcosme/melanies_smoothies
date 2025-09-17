64dc49298de57d604c44f6c29c3c6b4577a81550
danielperezcosme <danielperezcosme@gmail.com>
2025-09-17 17:09:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(f"Pending Smoothie Orders :cup_with_straw: {st.__version__}")
@@ -13,7 +13,10 @@ st.write(
 #Show list
 from snowflake.snowpark.functions import col 
 
-session = get_active_session()
+#session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 
 if my_dataframe:

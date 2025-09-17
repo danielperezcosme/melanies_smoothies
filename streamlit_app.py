6e9e485880cae83be33740be11cb8a88ecbaf040
danielperezcosme <danielperezcosme@gmail.com>
2025-09-17 18:51:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if ingredients_list:
         st.write('The search value for ', fruits_chosen,' is ', search_on, '.')
 
         st.subheader(fruits_chosen + " nutrition information")
-        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + search_on ) 
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_chosen ) 
         #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
